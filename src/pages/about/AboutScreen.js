29bc59ef54e18ff7fbf20e8977fbb8c44506872f
Sonubabut <111742766+Sonubabut@users.noreply.github.com>
2023-08-14 13:51:00 +0530

    Update AboutScreen.js

--- src/pages/about/AboutScreen.js
+++ src/pages/about/AboutScreen.js
@@ -8,45 +8,28 @@ function AboutScreen() {
         <div className="bg-white rounded-xl p-3 font-title">
           <PageTitle title="About Me" />
           <div className="mt-4 mb-5 flex flex-row items-center">
-            <img
-              src="https://raw.githubusercontent.com/lwinmoepaing/lwinmoepaing/main/img/gitto.gif"
-              className="h-12 mr-3"
-              alt="Git"
-            />
+            
             <div>
-              <a
-                href="https://github.com/lwinmoepaing"
-                target={"_blank"}
-                className="underline cursor-pointer"
-                rel="noreferrer"
-              >
-                Hi, I'm Lwin Moe Paing
+              
+                Hi, I'm SonubabuT
               </a>
-              <h1> Free Invoice Maker for your business.</h1>
+              <h1>Invoice for GBK</h1>
             </div>
           </div>
 
           <PageTitle title="Invoice Maker" />
           <div className="mt-2 pl-4 text-sm">
-            <ul class="list-disc">
-              <li> Can Easily Pre-Manage Your Products</li>
-              <li> Can Easily Pre-Manage Your Clients</li>
-              <li> Can Export PDF </li>
-              <li> Can Export Image </li>
-            </ul>
           </div>
           <div className="font-title mt-3 mb-5">
             <div>
-              🤝 I’m looking for help with JS dev to build Many Free Softwares
-              to help people who can't afford !!
             </div>
             <div>
               📫 How to reach me{" "}
               <a
-                href="mailto:lwinmoepaing.dev@gmail.com"
+                href="mailto:tsonubabu0@gmail.com"
                 className="underline cursor-pointer"
               >
-                lwinmoepaing.dev@gmail.com
+                tsonubabu0@gmail.com
               </a>{" "}
               (or){" "}
               <a
@@ -60,38 +43,28 @@ function AboutScreen() {
             </div>
             <div>
               <span>🎁 </span>
-              <a
-                href="https://github.com/lwinmoepaing/invoice-maker"
-                className="underline cursor-pointer"
-                target={"_blank"}
-                rel="noreferrer"
-              >
-                {" "}
-                Repo Link Here
-              </a>
+                
             </div>
           </div>
 
           <PageTitle title="Build By" />
           <div className="mt-2 mb-5 pl-4 text-sm">
             <ul class="list-disc">
-              <li> Framer Motion For each component Animation</li>
-              <li> Lottiefiles For Dashboard Widgets Icons</li>
-              <li> IndexedDB for Local Storage </li>
-              <li> ReactJS </li>
+              <li> SonubabuT</li>
+              <li> MERN Stack Developer</li>
             </ul>
           </div>
 
           <PageTitle title="Contact" />
           <div className="mt-2 pl-1 text-sm">
             <a
-              href="tel:+959420059241"
+              href="tel:+91 7339290039"
               className="underline cursor-pointer"
               target={"_blank"}
               rel="noreferrer"
             >
               {" "}
-              +959420059241
+              +917339290039
             </a>
           </div>
         </div>
